0da20ce8c4a421ed953cc370c30a4eb6a1e9a80e
Rafi <rafihassan190041234@gmail.com>
2021-02-14 20:07:42 +0600

    Added scrolling background and also change size of window

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 pygame.init()
 
 # Creating window
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((1080, 800))
 
 # FrameRate
 windowFrameRate = 60
@@ -15,18 +15,31 @@ pygame.display.set_caption("Earth Defender")
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
+# Scrolling Background
+background1Texture = pygame.image.load("images/background.png")
+background2Texture = pygame.image.load("images/background.png")
+background1Y = 0
+background2Y = -2690
+backgroundY_change = 1
+
 # Player
 playerTexture = pygame.image.load("images/Player1.png")
 playerX = 370
-playerY = 540
+playerY = 740
 playerX_change = 0
 
 # Enemy
 enemyTexture = pygame.image.load("images/enemy.png")
-enemyX = random.randint(0, 736)
-enemyY = random.randint(50, 150)
+enemyX = random.randint(0, 1034)
+enemyY = random.randint(0, 50)
 enemyX_change = 3
-enemyY_change = 1
+enemyY_change = 1.5
+
+
+# Drawing background
+def background(y1, y2):
+    window.blit(background1Texture, (0, y1))
+    window.blit(background2Texture, (0, y2))
 
 
 def player(x, y):
@@ -61,16 +74,27 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Background drawing
+    background1Y += backgroundY_change
+    background2Y += backgroundY_change
+    background(background1Y, background2Y)
+    if background2Y >= background2Texture.get_height():
+        background2Y = -background2Texture.get_height()
+    if background1Y >= background1Texture.get_height():
+        background1Y = -background1Texture.get_height()
+
+
+
     # Player boundary
     tempPositionX = playerX + playerX_change
-    if (tempPositionX > 0) and (tempPositionX < 736):
+    if (tempPositionX > 0) and (tempPositionX < 1034):
         playerX = tempPositionX
 
     player(playerX, playerY)
 
     # Enemy boundary and Movements
     tempPositionX = enemyX + enemyX_change
-    if (tempPositionX > 0) and (tempPositionX < 736):
+    if (tempPositionX > 0) and (tempPositionX < 1034):
         enemyX = tempPositionX
         enemyY += enemyY_change
     else:
